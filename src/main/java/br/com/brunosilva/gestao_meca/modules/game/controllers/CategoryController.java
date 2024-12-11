a0e9ddaa3d80762ecfcb17cd3de4291c08b81e79
Bruno Silva <silva.dev@hotmail.com>
2024-12-11 18:05:55 -0300

    Atualizando fonte category e company

---
+++ src/main/java/br/com/brunosilva/gestao_meca/modules/game/controllers/CategoryController.java
@@ -0,0 +1,30 @@
+package br.com.brunosilva.gestao_meca.modules.game.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.brunosilva.gestao_meca.modules.game.entities.CategoryEntity;
+import br.com.brunosilva.gestao_meca.modules.game.useCases.CreateCategoryUseCase;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+  @Autowired
+  private CreateCategoryUseCase createCategoryUseCase;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody CategoryEntity categoryEntity) {
+    try {
+      var result = this.createCategoryUseCase.execute(categoryEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+}
